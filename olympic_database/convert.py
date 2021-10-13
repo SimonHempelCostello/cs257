fef67b35867415ec0de7b03e0ee31a935bb41d00
hempel-costello.simon20@libertycommon.org <hempel-costello.simon20@libertycommon.org>
2021-10-13 18:38:21 -0500

    got conversion software working

--- olympic_database/convert.py
+++ olympic_database/convert.py
@@ -1,13 +1,85 @@
 import csv
 class Converter:
     def __init__(self) -> None:
+        self.olympian_event_dictionary = {}
+        self.event_NOC_dictionary = {}
         pass
     def read_in(self, athlete_event_file_name, NOC_file_name):
 
         self.athlete_event_list = []
         self.NOC_list = []
         with open(athlete_event_file_name) as file:
-            self.athlete_event_list = csv.reader(file)
+            csvreader = csv.reader(file)
+            for row in csvreader:
+                self.athlete_event_list.append(row)
         with open(NOC_file_name) as file:
-            self.NOC_list = csv.reader(file)
+            csvreader = csv.reader(file)
+            for row in csvreader:
+                self.NOC_list.append(row)
         pass
+    def write_olympians(self):
+        with open('olympians.csv', 'w', encoding = 'UTF8') as file:
+            writer = csv.writer(file)
+            output_row = ["id", "first name", "surname"]
+            writer.writerow(output_row)
+            last_id = ""
+            for a in self.athlete_event_list:
+                id = a[0]
+                full_name = a[1]
+                if(" " in full_name):
+                    first_name = full_name[: full_name.index(' ')]
+                    last_name = full_name[full_name.index(' ') :]
+                    output_row = [id , first_name,last_name]
+                    if(id != last_id):
+                        writer.writerow(output_row)
+                        last_id = id
+
+    def write_events(self):
+        output_list = []
+        with open('events.csv', 'w', encoding = 'UTF8') as file:
+            writer = csv.writer(file)
+            eventID = 0
+            for a in self.athlete_event_list:
+                id = str(a[0])
+                games = a[8]
+                year = a[9]
+                season = a[10]
+                city = a[11]
+                sport = a[12]
+                event = a[13]
+                medal = a[14]
+                sex = a[2]
+                age = a[3]
+                height = a[4]
+                mass = a[5]
+                NOC = a[7]
+                output_row = [eventID , games,year, season, city, sport, event, medal, sex, age, height, mass]
+                for entry in output_row:
+                    if entry == 'NA':
+                        print("hi")
+                        entry = 'Null'
+                eventID+=1
+                writer.writerow(output_row)
+                self.olympian_event_dictionary[eventID] = id
+                self.event_NOC_dictionary[eventID] = NOC
+    def write_olympian_events(self):
+        with open('olympians_events.csv', 'w', encoding = 'UTF8') as file:
+            writer = csv.writer(file)
+            for key in self.olympian_event_dictionary:
+                athlete_id = self.olympian_event_dictionary[key]
+                NOC = self.event_NOC_dictionary[key]
+                eventID = key
+                #athleteId = self.olympian_event_dictionary[eventID]
+                #NOC = self.event_NOC_dictionary[eventID]
+                output = [eventID, athlete_id, NOC]
+                writer.writerow(output)
+
+    
+if __name__ == "__main__":             
+    c = Converter()
+    c.read_in("athlete_events.csv", "noc_regions.csv")
+    c.write_olympians()
+    c.write_events()
+    c.write_olympian_events()
+
+
