34ba3316cc4060d5bf90556ea8876f61b854eaff
hempel-costello.simon20@libertycommon.org <hempel-costello.simon20@libertycommon.org>
2021-11-06 14:53:45 -0500

    added initial database design

---
+++ webapp/sql_commands/tweets-schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE tweets(
+    tweet_content text,
+    tweet_language text,
+    publish_date DATETIME, 
+    is_retweet integer,
+    tweet_id integer
+);
+CREATE TABLE authors(
+    external_author_id integer,
+    author_name text,
+    account_type text,
+    account_category text,
+    alt_external_id text
+);
+
+CREATE table tweet_instance(
+    tweet_id integer,
+    author_id integer,
+    accounts_followed integer,
+    followers integer
+);
