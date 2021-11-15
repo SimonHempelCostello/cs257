dbbbaf1ff8d64285ed5c1366b43a319e296c33ce
Lev <shusterl@carleton.edu>
2021-11-14 18:56:44 -0600

    removed the double hyperlinks from resaults

--- webapp/static/search.js
+++ webapp/static/search.js
@@ -37,7 +37,7 @@ function search_data_base() {
         }
         for (let k = 0; k < table_length; k++) {
             tweet = tweet_list[k];
-            table_body += "<li> <table style='width:100%' class = 'results-table'> <tr class = 'top-level-table-heading'> <th><a href='{{ url_for('follower_chart')}}'>Bot Account</a></th><td><a href='/follower_chart/"
+            table_body += "<li> <table style='width:100%' class = 'results-table'> <tr class = 'top-level-table-heading'> <th><a>Bot Account</a></th><td><a href='/follower_chart/"
             table_body += tweet['author_name']
             table_body += "'>" + tweet['author_name'] + "</a></td></tr>";
             table_body += generate_table_row('Followers', 'sorting_data', tweet);
